c8f060852796f3117d4efe27a773145e5c4f5091
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-08-28 16:32:36 +0200

    added SQL queries

--- vivinio_niko.py
+++ vivinio_niko.py
@@ -13,7 +13,24 @@ cursor.execute('''SELECT DISTINCT vintages.wine_id, vintages.name, wines.ratings
                LIMIT 10''')
 cursor.fetchall()
 
+
 # We have a marketing budget for this year. Which country should we prioritise and why?
+cursor.execute('''SELECT region_id, countries.name, regions.name, wines.name, vintages.name, wines.ratings_average, wines.ratings_count, users_count, vintages.price_euros
+               FROM wines
+               JOIN vintages ON wine_id == wines.id
+               JOIN regions ON region_id == regions.id
+               JOIN countries ON regions.country_code == countries.code
+               GROUP BY countries.code
+               ORDER BY users_count DESC, vintages.ratings_count DESC, vintages.ratings_average DESC
+               LIMIT 10''')
+data = cursor.fetchall()
+
+
+
 # We would like to give awards to the best wineries. Come up with 3 relevant ones. Which wineries should we choose and why? Be creative ;)
+
+
 # We have detected that a big cluster of customers like a specific combination of tastes. We have identified a few primary keywords that match this.
+
+
 # We have detected that a big cluster of customers like a specific combination of tastes. We have identified a few primary keywords that match this. We would like you to find all the wines that have those keywords. To ensure the accuracy of our selection, ensure that more than 10 users confirmed those keywords. Also, identify the group_name related to those keywords.
